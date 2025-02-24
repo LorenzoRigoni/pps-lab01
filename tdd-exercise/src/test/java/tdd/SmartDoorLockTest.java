d7ca8e03f83543ad7ea41d9fec342fab8f055068
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-02-24 11:36:57 +0100

    [EX2] Created tests for reset and pin not set

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -9,8 +9,10 @@ public class SmartDoorLockTest {
 
     private static final int RIGHT_LOCK_PIN = 1234;
     private static final int WRONG_LOCK_PIN = 4321;
+    private static final int INITIAL_NUM_ATTEMPTS = 0;
     private static final int FIRST_ATTEMPT = 1;
     private static final int MAX_ATTEMPTS = 3;
+    private static final String PIN_NOT_SET_MESSAGE = "The PIN is not set";
     private SmartDoorLock lock;
 
     @BeforeEach
@@ -56,5 +58,28 @@ public class SmartDoorLockTest {
                 () -> assertTrue(lock.isBlocked()),
                 () -> assertEquals(MAX_ATTEMPTS, lock.getFailedAttempts())
         );
+    }
+
+    @Test
+    void testResetLock() {
+        lock.setPin(RIGHT_LOCK_PIN);
+        try {
+            lock.lock();
+        } catch (Exception e) {}
+
+        lock.unlock(WRONG_LOCK_PIN);
+
+        lock.reset();
+        assertAll(
+                () -> assertEquals(INITIAL_NUM_ATTEMPTS, lock.getFailedAttempts()),
+                () -> assertFalse(lock.isLocked()),
+                () -> assertFalse(lock.isBlocked())
+        );
+    }
+
+    @Test
+    void testExceptionPinNotSet() {
+        Exception exception = assertThrows(Exception.class, () -> lock.lock());
+        assertEquals(PIN_NOT_SET_MESSAGE, exception.getMessage());
     }
 }
