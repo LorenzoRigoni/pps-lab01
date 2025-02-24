a3803b2056544f7aaa3f193e13e8e702469ca2ff
Lorenzo Rigoni <lorenzo.rigoni2@studio.unibo.it>
2025-02-24 11:45:54 +0100

    [EX2] Created tests for set pin when lock is locked or blocked

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -11,13 +11,14 @@ public class SmartDoorLockTest {
     private static final int WRONG_LOCK_PIN = 4321;
     private static final int INITIAL_NUM_ATTEMPTS = 0;
     private static final int FIRST_ATTEMPT = 1;
-    private static final int MAX_ATTEMPTS = 3;
+    private static int MAX_ATTEMPTS;
     private static final String PIN_NOT_SET_MESSAGE = "The PIN is not set";
     private SmartDoorLock lock;
 
     @BeforeEach
     void beforeEach() {
         this.lock = new SmartDoorLockImpl();
+        MAX_ATTEMPTS = this.lock.getMaxAttempts();
     }
 
     @Test
@@ -81,5 +82,42 @@ public class SmartDoorLockTest {
     void testExceptionPinNotSet() {
         Exception exception = assertThrows(Exception.class, () -> lock.lock());
         assertEquals(PIN_NOT_SET_MESSAGE, exception.getMessage());
+    }
+
+    @Test
+    void testSetPinWhenIsLocked() {
+        lock.setPin(RIGHT_LOCK_PIN);
+
+        try {
+            lock.lock();
+        } catch (Exception e) {}
+
+        lock.setPin(WRONG_LOCK_PIN);
+        lock.unlock(WRONG_LOCK_PIN);
+
+        assertAll(
+                () -> assertEquals(FIRST_ATTEMPT, lock.getFailedAttempts()),
+                () -> assertTrue(lock.isLocked())
+        );
+    }
+
+    @Test
+    void testSetPinWhenIsBlocked() {
+        lock.setPin(RIGHT_LOCK_PIN);
+
+        try {
+            lock.lock();
+        } catch (Exception e) {}
+
+        lock.setPin(WRONG_LOCK_PIN);
+
+        for (int i = 0; i < MAX_ATTEMPTS; i++)
+            lock.unlock(WRONG_LOCK_PIN);
+
+        assertAll(
+                () -> assertTrue(lock.isLocked()),
+                () -> assertTrue(lock.isBlocked()),
+                () -> assertEquals(MAX_ATTEMPTS, lock.getFailedAttempts())
+        );
     }
 }
